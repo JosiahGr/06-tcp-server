a7d82b3c1db495d9d8234bffb3303e6c0c83592b
Josiah Green <JosiahGreen90@gmail.com>
2018-04-28 17:18:24 -0700

    made a unique id function

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const faker = require('faker');
-const uuid = require('uuid');
 
 module.exports = class Client {
   constructor(socket) {
     this.nickname = faker.internet.userName();
-    this.userId = uuid();
     this.socket = socket;
+    this.userId = function uniqueId() {
+      const id = new Date();
+      return id;
+    };
   }
 };
